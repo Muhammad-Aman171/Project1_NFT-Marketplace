a466037bd695d7ea0b16a5960fbe74dcf98a3cc2
Muhammad-Aman171 <ag3734359@gmail.com>
2024-09-08 03:34:55 +0500

    working

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import Image from 'next/image';
 
-export default function SignUp() {
+import Img1 from '@/public/assets/Image Placeholder.png'
+import UserSvg from '../components/svgs/user';
+
+const SignUp:React.FC =()=> {
   return (
-    <div>SignUp</div>
+    <div className="border h-[100vh] " >
+      <Image
+        src={Img1}
+        alt="img does not show"
+      />
+      <div>
+        <h1>Create account</h1>
+        <p>Welcome! enter your details and start creating, collecting and selling NFTs.</p>
+        <div>
+          <UserSvg/>
+          <input type="text" />
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <button></button>
+      </div>
+    </div>
   )
 }
+export default SignUp;
