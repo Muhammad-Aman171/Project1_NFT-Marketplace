f57036017ff39dfbb6a816d8ad8cad438ec6fc87
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-06 02:47:57 +0500

    working

--- app/components/homePageComponents/topCreatorsCards/topCreatorsCard.tsx
+++ app/components/homePageComponents/topCreatorsCards/topCreatorsCard.tsx
@@ -9,15 +9,17 @@ import './topCreator.scss'
 
 const TopCreatorsCards: React.FC<TopCreatorsTypes> = ({style, srcImg,name}) => {
   return (
-    <div className={`${style} top-creators-cards rounded-[1vw] bg-[#3B3B3B] w-[18vw] h-[40vh] flex flex-col  items-center justify-evenly     max-md:w-[37vw] max-md:border max-md:flex-row max-md:h-[15vh]  max-md:rounded-[3vw] `} >
-      <Image 
-        src={srcImg!}
-        alt="Img does note show"
-        className="bg-[#3B3B3B]   max-md:w-[7vw]  "
-      />
-      <div className=" bg-[#3B3B3B]  flex flex-col items-center  max-md:items-start">
-        <h1 className="bg-[#3B3B3B]  max-md:text-[2.5vw] max-md:font-semibold  ">{name}</h1>
-        <p className="text-[#858584] bg-[#3B3B3B] ">Total Sales: <span className=" bg-[#3B3B3B] ">34.53 ETH</span> </p>
+    <div className={`${style} top-creators-cards `} >
+      <div className="top-creators-img">
+        <Image 
+          src={srcImg!}
+          alt="Img does note show"
+          className="top-creators-img1"
+        />
+      </div>
+      <div className="top-creators-headings bg-[#3B3B3B]  flex flex-col items-center  max-md:items-start">
+        <h1 className=" ">{name}</h1>
+        <p className="">Total Sales: <span className=" bg-[#3B3B3B] ">34.53 ETH</span> </p>
       </div>
     </div>
   )
