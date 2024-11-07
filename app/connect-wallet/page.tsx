9c82576934fb7663b695a2c1b6cc62edbd88b3f0
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-07 21:11:53 +0500

    working

--- app/connect-wallet/page.tsx
+++ app/connect-wallet/page.tsx
@@ -6,47 +6,61 @@ import Img2 from "@/public/assets/connet wallet assets/Metamask.png";
 import Img3 from "@/public/assets/connet wallet assets/WalletConnect.png";
 import Img4 from "@/public/assets/connet wallet assets/Coinbase.png";
 
+import "./connect-wallet.scss";
 
-const SignUp: React.FC = () => {
+const ConnectWallet: React.FC = () => {
   return (
-    <div className=" h-[130vh] w-[98.5vw] flex items-center justify-between  ">
-      <Image
-        src={Img1}
-        alt="img does not show"
-        className=" w-[45vw] h-[130vh] "
-      />
-      <div className=" w-[50vw] h-[100vh] flex flex-col justify-evenly ">
-        <h1 className="text-[4vw] font-semibold  ">Connect wallet</h1>
-        <p className="text-[2vw] font-extralight w-[70%] ">
-          Choose a wallet you want to connect. There are several wallet
-          providers.
-        </p>
-        <div className="border-[.1vw] border-[#A259FF] flex items-center pl-[4vw] bg-[#3B3B3B] rounded-[1.7vw] w-[22vw] h-[12vh] ">
-          <Image
-            src={Img2}
-            alt="img does not show"
-            className="bg-[#3B3B3B]  "
-          />
-          <h1 className="bg-[#3B3B3B] outline-none ml-[1vw] text-white text-[1.5vw] font-semibold ">   Metamask</h1>
+    <div className="connect-wallet">
+      <div className="inner-connect-wallet">
+        <div className="connect-wallet-img">
+          <Image src={Img1} alt="img does not show" className="img1" />
         </div>
-        <div className=" border-[.1vw] border-[#A259FF] flex items-center pl-[4vw] bg-[#3B3B3B] rounded-[1.7vw] w-[22vw] h-[12vh] ">
-        <Image
-            src={Img3}
-            alt="img does not show"
-            className="bg-[#3B3B3B]  "
-          />
-          <h1 className="bg-[#3B3B3B] outline-none ml-[1vw] text-white text-[1.5vw] font-semibold ">   Wallet Connect</h1>
+        <div className="connect-wallet-div2">
+          <div className="connect-wallet-heading">
+            <h1>Connect wallet</h1>
+            <p>
+              Choose a wallet you want to connect. There are several wallet
+              providers.
+            </p>
+          </div>
+          <div className="connect-wallet-buttons">
+            <div>
+              <Image
+                src={Img2}
+                alt="img does not show"
+                className="img2 bg-[#3B3B3B]  "
+              />
+              <h1 className="bg-[#3B3B3B] outline-none ml-[1vw] text-white text-[1.5vw] font-semibold ">
+                {" "}
+                Metamask
+              </h1>
+            </div>
+            <div>
+              <Image
+                src={Img3}
+                alt="img does not show"
+                className="img2 bg-[#3B3B3B]  "
+              />
+              <h1 className="bg-[#3B3B3B] outline-none ml-[1vw] text-white text-[1.5vw] font-semibold ">
+                {" "}
+                Wallet Connect
+              </h1>
+            </div>
+            <div>
+              <Image
+                src={Img4}
+                alt="img does not show"
+                className="img2 bg-[#3B3B3B]  "
+              />
+              <h1 className="bg-[#3B3B3B] outline-none ml-[1vw] text-white text-[1.5vw] font-semibold ">
+                {" "}
+                Coinbase
+              </h1>
+            </div>
+          </div>
         </div>
-        <div className="border-[.1vw] border-[#A259FF] flex items-center pl-[4vw] bg-[#3B3B3B] rounded-[1.7vw] w-[22vw] h-[12vh] ">
-        <Image
-            src={Img4}
-            alt="img does not show"
-            className="bg-[#3B3B3B]  "
-          />
-          <h1 className="bg-[#3B3B3B] outline-none ml-[1vw] text-white text-[1.5vw] font-semibold ">   Coinbase</h1>
-        </div> 
       </div>
     </div>
   );
 };
-export default SignUp;
+export default ConnectWallet;
