d3328402c3cb0bfe6989bf33fd4195c09b4c920e
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-08 18:48:16 +0500

    working

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -13,52 +13,32 @@ const SignUp: React.FC = () => {
     <div className="sign-up">
       <div className="inner-sign-up">
         <div className="sign-up-img">
-          <Image
-            src={Img1}
-            alt="img does not show"
-            className="img1"
-          />
+          <Image src={Img1} alt="img does not show" className="img1" />
         </div>
         <div className="sign-up-div2">
           <div className="inner-sign-up-div2">
             <div className="sign-up-div2-heading">
               <h1 className="text-[4vw] font-semibold  ">Create account</h1>
               <p className="text-[2vw] font-extralight w-[70%] ">
-                Welcome! enter your details and start creating, collecting and selling
-                NFTs.
+                Welcome! enter your details and start creating, collecting and
+                selling NFTs.
               </p>
             </div>
-            <div className="input-field flex items-center bg-white rounded-[1.7vw] w-[25vw] h-[9vh] ">
-              <User2Svg style="w-[3vw] bg-white rounded-l-[1.7vw] pl-[1vw]  h-[9vh] text-[#BDBDBD] " />
-              <input
-                className="bg-white outline-none ml-[1vw] text-black text-[1.5vw] placeholder:text-[#2B2B2B] "
-                type="text"
-                placeholder="Username"
-              />
+            <div className="input-field ">
+              <User2Svg style="w-[20px] h-[20px]" />
+              <input type="text" placeholder="Username" />
             </div>
-            <div className="input-field flex items-center bg-white rounded-[1.7vw] w-[25vw] h-[9vh] ">
-              <EmailSvg style="w-[3vw] bg-white rounded-l-[1.7vw] pl-[1vw]  h-[9vh] text-[#BDBDBD] " />
-              <input
-                className="bg-white outline-none ml-[1vw] text-black text-[1.5vw] placeholder:text-[#2B2B2B] "
-                type="emial"
-                placeholder="Email Address"
-              />
+            <div className="input-field ">
+              <EmailSvg style="w-[20px] h-[20px] " />
+              <input type="emial" placeholder="Email Address" />
             </div>
-            <div className="input-field flex items-center bg-white rounded-[1.7vw] w-[25vw] h-[9vh]">
-              <LockSvg style="w-[3vw] bg-white rounded-l-[1.7vw] pl-[1vw]  h-[9vh] text-[#BDBDBD] " />
-              <input
-                className="bg-white outline-none ml-[1vw] text-black text-[1.5vw] placeholder:text-[#2B2B2B] "
-                type="password"
-                placeholder="Password"
-              />
+            <div className="input-field ">
+              <LockSvg style="w-[20px] h-[20px]" />
+              <input type="password" placeholder="Password" />
             </div>
-            <div className="input-field flex items-center bg-white rounded-[1.7vw] w-[25vw] h-[9vh]">
-              <LockSvg style="w-[3vw] bg-white rounded-l-[1.7vw] pl-[1vw]  h-[9vh] text-[#BDBDBD] " />
-              <input
-                className="bg-white outline-none ml-[1vw] text-black text-[1.5vw] placeholder:text-[#2B2B2B] "
-                type="password"
-                placeholder="Confirm Password"
-              />
+            <div className="input-field">
+              <LockSvg style="w-[20px] h-[20px] bg-white " />
+              <input type="password" placeholder="Confirm Password" />
             </div>
             <div className="sign-up-button">
               <button className="bg-[#A259FF] w-[25vw] h-[9vh] rounded-[1.7vw] text-[1.5vw] font-semibold ">
