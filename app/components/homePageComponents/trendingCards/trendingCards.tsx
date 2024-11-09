bc4e7e657f58202306629b6d6570692a4304ed69
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-09 23:52:05 +0500

    working

--- app/components/homePageComponents/trendingCards/trendingCards.tsx
+++ app/components/homePageComponents/trendingCards/trendingCards.tsx
@@ -27,7 +27,7 @@ const TrendingCards: React.FC<TrendingCollectionTypes> = ({
           <Image src={srcImg1!} alt="img1 does not show" />
         </div>
         <div className="small-imgs">
-          <div className="img2">
+          <div className="img">
             <Image src={srcImg2!} alt="img1 does not show" />
           </div>
           <div className="img3">
