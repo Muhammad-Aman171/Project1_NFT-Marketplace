c20fece07f86c0718a156af022ef105f8ffe5cea
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-10 12:45:25 +0500

    working

--- app/components/homePageComponents/browseCategories/browseCategories.tsx
+++ app/components/homePageComponents/browseCategories/browseCategories.tsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import Image from 'next/image'
-import { BrowseCategoriesTypes } from '@/types/homePage/homePage'
-import './browseCategories.scss'
+import React from "react";
+import Image from "next/image";
+import { BrowseCategoriesTypes } from "@/types/homePage/homePage";
+import "./browseCategories.scss";
 
-const BrowseCategories: React.FC<BrowseCategoriesTypes> = ({srcImg,name}) => {
-  return ( 
+const BrowseCategories: React.FC<BrowseCategoriesTypes> = ({
+  srcImg,
+  name,
+}) => {
+  return (
     <div className="browse-categories ">
       <div className="inner-browse-categories">
-        <Image
-          src={srcImg!}
-          alt="img does not show"
-          
-        />
-  
-        <p >{name}</p>
+        <Image src={srcImg!} alt="img does not show" className="img1" />
+        <p>{name}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 export default BrowseCategories;
