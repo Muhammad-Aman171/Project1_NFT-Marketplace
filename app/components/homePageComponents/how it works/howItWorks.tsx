27916b15d438f4038e84cec5458910ac1ba1d990
Muhammad-Aman171 <ag3734359@gmail.com>
2024-11-12 12:36:51 +0500

    working

--- app/components/homePageComponents/how it works/howItWorks.tsx
+++ app/components/homePageComponents/how it works/howItWorks.tsx
@@ -9,8 +9,10 @@ const HowItWorks: React.FC<HowItWorksTypes> = ({ srcImg, heading, para }) => {
     <div className="how-it-work ">
       <div>
         <div className="how-img1"><Image src={srcImg!} alt="img does not show" className=" img1" /></div>
-        <h1>{heading}</h1>
-        <p>{para}</p>
+       <div className=" how-it-work-heading" >
+          <h1>{heading}</h1>
+          <p>{para}</p>
+       </div>
       </div>
     </div>
   );
